d4adf29b8f9f45e5f15831668f7c15c336662e8d
Mohammed Shurrab <devmsh87@gmail.com>
2018-10-29 23:03:19 +0200

    fixes

--- database/seeds/CoursesFilesSeeder.php
+++ database/seeds/CoursesFilesSeeder.php
@@ -12,39 +12,39 @@ class CoursesFilesSeeder extends Seeder
     public function run()
     {
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '1',
         ]);
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '2',
         ]);
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '3',
         ]);
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '4',
         ]);
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '5',
         ]);
         App\CoursesFiles::create([
-            'video_title' => '["Health Science","Health and Social Care","Health Science","Health and Social Care"]',
-            'video_category' => '["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"]',
-            'video_url' => '["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"]',
+            'video_title' => ["Health Science","Health and Social Care","Health Science","Health and Social Care"],
+            'video_category' => ["Introdocution","Generative Modeling Review","Variational Autoencoders","Gaussian Mixture Model Review"],
+            'video_url' => ["https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw","https://www.youtube.com/watch?v=LDgd_gUcqCw"],
             'course_id' => '6',
         ]);
     }
